cc059c0b2f43e8c2e738cb955efb1aa01deb32b9
iamdeepz <deepakchandrasekaran14@gmail.com>
2022-05-29 12:26:50 +0530

    swagger api setup completed

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
 const app = express();
+const swaggerUI = require('swagger-ui-express');
+const yaml = require('yamljs');
+const swaggerDocument = yaml.load('./swagger.yaml');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
+
 
 
 // IMPORT ALL ROUTES
 const homeRoutes = require('./routers/homeRoutes');
 
 // Middlewares
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use(express.json()); // TO RECOGNISE THE REQ OBJECT AS JSON
 app.use(express.urlencoded({extended: true})); // TO RECOGNISE THE REQ OBJECT AS STRINGS AND ARRAYS
 app.use(cookieParser());
@@ -15,7 +20,7 @@ app.use(morgan('tiny')); // HTTP REQUEST LOGGER
 
 
 //ROUTER MIDDLEWARE
-app.use('/api/v1/home/', homeRoutes);
+app.use('/api/home/', homeRoutes);
 
 // Export app.js
 module.exports = app;
