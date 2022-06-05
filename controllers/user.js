f57dca6dfc45ee113c04fe7dd8d4c3a9d309ad34
iamdeepz <deepakchandrasekaran14@gmail.com>
2022-06-05 16:22:24 +0530

    changepassword and update user details completed

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
 const User = require('../models/user');
+const cookieToken = require('../utils/cookieToken');
+const cloudinary = require('cloudinary');
 
 exports.getuser = (req, res) => {
     try {
@@ -6,6 +8,73 @@ exports.getuser = (req, res) => {
             success: true,
             user: req.user
         });
+    } catch (err) {
+        res.status(400).send({error: err.message});
+    }
+}
+
+exports.changePassword = async(req, res) => {
+    try {
+        // Get the old and new password
+        const { oldPassword, newPassword } = req.body;
+
+        // Get the user from the DB
+        const user = await User.findById(req.user._id).select('+password');;
+       
+        // validating the old password
+        const is_correct_password = await user.comparePassword(oldPassword);
+       
+        if(!is_correct_password){
+            throw new Error('Please provide a valid password');
+        }
+
+        user.password = newPassword;
+
+        await user.save();
+
+        cookieToken(user, res);
+    } catch (err) {
+        res.status(400).send({error: err.message});
+    }
+}
+
+exports.updateUser = async(req, res) => {
+    try {
+        const { name, email} = req.body;
+
+        let updatePayload = {
+            name,
+            email
+        };
+
+        // photo updation
+        if(req.files){
+            // Remove the Image from the cloudinary
+            await cloudinary.v2.uploader.destroy(req.user.photo.id);
+
+            // Upload new Image to the cloudinary
+            const fileUpload = await cloudinary.v2.uploader.upload(req.files.photo.tempFilePath,{
+                folder: "users",
+                width: 150,
+                crop: "scale"
+            });
+
+            updatePayload.photo = {
+                id: fileUpload.public_id, 
+                secure_url: fileUpload.secure_url
+            }
+        }
+
+        const user = await User.findByIdAndUpdate(req.user._id, updatePayload, {
+            new: true,
+            runValidators: true
+        });
+
+        res.status(200).json({
+            success: true,
+            user: user
+        })
+
     } catch (err) {
         res.status(400).send({error: err.message});
     }
--- routers/user.js
+++ routers/user.js
@@ -3,9 +3,15 @@ const router = express.Router();
 const {verifyUser} = require('../middlewares/userVerify');
 
 const { 
-   getuser
+   getuser,
+   changePassword,
+   updateUser
 } = require('../controllers/user');
 
 router.get('/userinfo', verifyUser, getuser);
+
+router.post('/changepassword', verifyUser, changePassword);
+
+router.post('/updateUser', verifyUser, updateUser);
 
 module.exports = router
