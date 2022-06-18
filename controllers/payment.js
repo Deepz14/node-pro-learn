f87fc3654e1d7e4e23a31df705a712ee941f69c7
iamdeepz <deepakchandrasekaran14@gmail.com>
2022-06-18 12:58:11 +0530

    Razor pay controller added

--- controllers/payment.js
+++ controllers/payment.js
@@ -25,9 +25,41 @@ exports.stripePayment = async(req, res) => {
 
         res.status(200).json({
             success: true,
+            amount: req.body.amount,
             client_secret: paymentIntent.client_secret
         })
 
+    } catch (err) {
+        res.status(400).send({error: err.message});
+    }
+}
+
+exports.sendRazorPayKey = async(req, res) => {
+    try {
+        res.status(200).json({
+            success: true,
+            razorpay_key: process.env.RAZORPAY_API_KEY
+        });
+
+    } catch (err) {
+        res.status(400).send({error: err.message});
+    }
+}
+
+exports.razorPayment = async(req, res) => {
+    try {
+        
+        let instance = new Razorpay({ key_id: process.env.RAZORPAY_API_KEY, key_secret: process.env.RAZORPAY_SECRET_KEY});
+
+        let options = {  amount: req.body.amount,  currency: "INR"}
+
+        const myOrder = await instance.orders.create(options);
+        
+        res.status(200).json({
+            success: true,
+            amount: req.body.amount,
+            order: myOrder
+        })
     } catch (err) {
         res.status(400).send({error: err.message});
     }
